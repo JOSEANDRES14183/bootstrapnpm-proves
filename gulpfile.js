3cc5fb00933df20b75774b1595758228a5a2476f
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2021-12-10 13:02:32 +0100

    a

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,7 @@ var sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
 };
 
-function buildStyles2() {
-    return gulp.src('./scss/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./lib/bootstrap-5.1.3/dist/css'));
-  };
-
-exports.buildStyles = buildStyles;
-exports.buildStyles2 = buildStyles2;
+exports.build = buildStyles;
